72057ff71ec2f4c0eb5c18f55f3833df71dcbf40
Ashwin Suryawanshi <a.suryawanshi@globant.com>
2017-12-26 16:29:27 +0530

    Added script to check naming convention guidelines for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
 
 
 @NgModule({
--- testscript.js
+++ testscript.js
@@ -1,9 +1,44 @@
 var fs = require('fs');
-var files = fs.readdirSync('src/app/');
-determinePaths(files);
+var path = require('path');
+var errCount = 0;
 
-function determinePaths(files) {
-    console.log(files);
-    return files;
-};
+/*This function accepts directory path & file types as arguments & returns an array with list of
+all the matching files found within the directory.
+1st parameter should be the directory path in string format
+2nd parameter should be an array of extensions of the file types that you want to search for.
+It should be an array of strings. */
+function getFilesFromDir(dir, fileTypes) {
+  var filesToReturn = [];
+  function walkDir(currentPath) {
+    var files = fs.readdirSync(currentPath);
+    for (var i in files) {
+      var curFile = path.join(currentPath, files[i]);      
+      if (fs.statSync(curFile).isFile() && fileTypes.indexOf(path.extname(curFile)) != -1) {
+        filesToReturn.push(curFile.replace(dir, ''));
+      } else if (fs.statSync(curFile).isDirectory()) {
+       walkDir(curFile);
+      }
+    }
+  };
+  walkDir(dir); 
+  return filesToReturn;
+}
 
+/*This function checks the naming convention for components*/
+function checkComponentNamingFormat() {
+  getFilesFromDir("src/app/components", [".ts",".html",".css"]).forEach(function(item, index) {
+    var componentName = path.basename(path.dirname(item)) + '.component';
+    var fileName = (item.split(/[\\/]/).pop()).replace(/.*\/|\.[^.]*$/g, '');
+    if(componentName.indexOf(fileName) >= 0) {
+      console.log("PASS: ", item);
+    } else {
+      console.log("FAIL: ", item);
+      errCount++;
+    }
+  });
+  if (errCount) {
+    throw 'ERROR: ' + errCount + ' file(s) have incorrect file name. Please follow the coding guidelines.';
+  }
+}
+
+checkComponentNamingFormat();
